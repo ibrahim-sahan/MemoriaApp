a6ec2b73a8eec3147531b2af7e8dc8deddc59b8e
ibrahim-sahan <ibrsahan8@gmail.com>
2024-03-14 20:01:03 +0300

    Initial Commit

---
+++ MemoriaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoriaApp
+//
+//  Created by İbrahim Şahan on 14.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
